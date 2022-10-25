f2203f8c0805091cae6df26af28eab3a98215386
Gavin Williams <gavin.williams@depop.com>
2022-10-25 14:52:11 +0100

    Various action updates
    This commit is a general maintenance and refactor
    
    * Upgrade dependencies
    * Bump NodeJS version to `16`
    * Clean-up code and update logic t oreflect new filename format
    * Add a `Continous Integration` workflow
    * Commit `dist` packaged js

--- index.js
+++ index.js
@@ -1,103 +1,129 @@
-const fs = require('fs')
-const os = require('os')
-const path = require('path')
-const process = require('process')
+const os = require("os");
 
-const core = require('@actions/core')
-const tc = require('@actions/tool-cache')
-const { Octokit } = require('@octokit/rest')
+const core = require("@actions/core");
+const tc = require("@actions/tool-cache");
+const { Octokit } = require("@octokit/rest");
 
 async function run() {
-  if (isSupportedPlatform(process.platform)) {
-    const version = await getTerraformDocsVersion()
-    const url = getDownloadUrl(version, process.platform)
+  // Gather OS details
+  const osPlatform = os.platform();
+  const osArch = os.arch();
 
-    const targetPath = getTargetPath()
-    core.debug(`Downloading Terraform docs version [${version}] for platform [${process.platform}] from [${url}] to destination [${targetPath}]`)
-    const terraformDocsPath = await tc.downloadTool(url, targetPath)
+  if (isSupportedPlatform(osPlatform)) {
+    try {
+      const version = await getTerraformDocsVersion();
 
-    makeFileExecutable(terraformDocsPath)
+      const platform = mapOS(osPlatform);
+      const arch = mapArch(osArch);
+      const url = getDownloadUrl(version, platform, arch);
+      core.debug(
+        `Downloading Terraform docs version [${version}] for platform [${platform}-${arch}] from [${url}]`
+      );
 
-    core.debug(`Adding [${getTargetDirectory()}] into PATH`)
-    core.addPath(getTargetDirectory())
+      const terraformDocsDownload = await tc.downloadTool(url);
+
+      if (!terraformDocsDownload) {
+        throw new Error(`Unable to download Terraform-docs from ${url}`);
+      }
+
+      let terraformDocsPath = "";
+
+      if (platform == "windows") {
+        terraformDocsPath = await tc.extractZip(terraformDocsDownload);
+      } else {
+        terraformDocsPath = await tc.extractTar(terraformDocsDownload);
+      }
+
+      if (!terraformDocsPath) {
+        throw new Error(`Unable to extract Terraform-docs archive`);
+      }
+
+      core.debug(`Adding [${terraformDocsPath}] into PATH`);
+      core.addPath(terraformDocsPath);
+    } catch (error) {
+      core.error(error);
+      throw error;
+    }
   }
 }
 
-function getTargetDirectory() {
-  return path.join(os.homedir(), 'terraform_docs', 'bin')
+// arch in [arm, x32, x64...] (https://nodejs.org/api/os.html#os_os_arch)
+// return value in [amd64, 386, arm]
+function mapArch(arch) {
+  const mappings = {
+    x32: "386",
+    x64: "amd64",
+  };
+  return mappings[arch] || arch;
 }
 
-function getTargetPath() {
-  return path.join(getTargetDirectory(), 'terraform-docs')
+// os in [darwin, linux, win32...] (https://nodejs.org/api/os.html#os_os_platform)
+// return value in [darwin, linux, windows]
+function mapOS(os) {
+  const mappings = {
+    win32: "windows",
+  };
+  return mappings[os] || os;
+}
+
+function mapOSExtension(os) {
+  const mappings = {
+    windows: "exe",
+    linux: "tar.gz",
+    darwin: "tar.gz",
+  };
+  return mappings[os];
 }
 
 function isSupportedPlatform(platform) {
-  const supportedPlatforms = ['win32', 'linux', 'darwin']
+  const supportedPlatforms = ["win32", "linux", "darwin"];
   if (supportedPlatforms.includes(platform)) {
     return true;
   } else {
     throw new Error(
       `Your platform (${platform}) is not supported by the action.
       Supported platforms: ${supportedPlatforms}`
-    )
+    );
   }
-}
-
-function isWindows() {
-  return process.platform == 'win32'
 }
 
 function getOctokit() {
-  const options = {}
-  const token = core.getInput("token")
+  const options = {};
+  const token = core.getInput("token");
   if (token) {
-    core.debug("Using token authentication for Octokit")
-    options.auth = token
+    core.debug("Using token authentication for Octokit");
+    options.auth = token;
   }
-  return new Octokit(options)
+  return new Octokit(options);
 }
 
 async function getTerraformDocsVersion() {
-  const inputVersion = core.getInput("terraform_docs_version", {required: true})
+  const inputVersion = core.getInput("terraform_docs_version", {
+    required: true,
+  });
   if (inputVersion == "latest") {
-    core.debug("Requesting for [latest] version ...")
-    const octokit = getOctokit()
+    core.debug("Requesting for [latest] version ...");
+    const octokit = getOctokit();
     const response = await octokit.repos.getLatestRelease({
-      owner: 'terraform-docs',
-      repo: 'terraform-docs'
-    })
-    core.debug(`... version resolved to [${response.data.name}`)
-    return response.data.name
+      owner: "terraform-docs",
+      repo: "terraform-docs",
+    });
+    core.debug(`... version resolved to [${response.data.name}`);
+    return response.data.name;
   } else {
-    return inputVersion
+    return inputVersion;
   }
 }
 
-function getDownloadUrl(version, platform) {
-  const baseUrl = "https://github.com/terraform-docs/terraform-docs/releases/download"
-  const fileName = getDownloadFilename(version, platform)
-  return `${baseUrl}/${version}/${fileName}`
+function getDownloadUrl(version, platform, arch) {
+  const baseUrl =
+    "https://github.com/terraform-docs/terraform-docs/releases/download";
+  const extension = mapOSExtension(platform);
+  const fileName = `terraform-docs-${version}-${platform}-${arch}.${extension}`;
+  return `${baseUrl}/${version}/${fileName}`;
 }
-
-function getDownloadFilename(version, platform) {
-  const fileNamePlatformMatrix = {
-    win32: 'windows-amd64.exe',
-    darwin: 'darwin-amd64',
-    linux: 'linux-amd64'
-  }
-  return `terraform-docs-${version}-${fileNamePlatformMatrix[platform]}`
-}
-
-function makeFileExecutable(filename) {
-  if (!isWindows()) {
-    const chmod = '755'
-    core.debug(`Setting chmod [${filename}] to [${chmod}]`)
-    fs.chmodSync(`${filename}`, chmod)
-  }
-}
-
 
 // RUN THE ACTION
-run().catch((error) => core.setFailed(error.message))
+run().catch((error) => core.setFailed(error.message));
 
-module.exports = { run, makeFileExecutable }
+module.exports = { run };
